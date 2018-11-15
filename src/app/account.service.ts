14851acf03c570cbeaa18034f032dd97eb5a643d
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2018-11-15 16:39:55 +0000

    Began implementation of registration/login

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
 import { Account } from './account';
+import { LoginComponent } from './login/login.component';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+const httpOpts = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class AccountService {
-  private apiUrlRoute = 'Account';
-  constructor() { }
+  private wsUrl = 'https://cc18594.whatsalon.com/api/Account';
+  constructor(private hClient: HttpClient) { }
+
+  registerUser(newUser: Account): Observable<Account> {
+    return this.hClient.post<Account>(`${this.wsUrl}/register`, newUser,).pipe(
+      catchError(this.handleError('registerUser', []))
+    );
+  }
+
+  login(emailAddr: string, pass: string) {
+    //TODO: Finish login logic
+    return this.hClient.post<any>(`${this.wsUrl}/login`, {emailAddr, pass}).pipe(
+      map(Account => {
+        if(success){
+
+        }
+      })
+    )
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Account } from '../account';
 import { FormBuilder, FormGroup, FormCont, FormControl, FormControl, FormControlrol, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -8,16 +10,21 @@ import { FormBuilder, FormGroup, FormCont, FormControl, FormControl, FormControl
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  @Input() newUser: Account;
+  //Adding a form for the user to fill out
   profileForm = new FormGroup({
-    email: new FormControl('example@domain.com'),
+    emailAddr: new FormControl('example@domain.com'),
     name: new FormControl(''),
     username: new FormControl(''),
     pWord: new FormControl('********'),
   });
 
-  constructor() { }
+  constructor(private acService: AccountService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
 }
